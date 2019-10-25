152cf0f58709c805374776bcd3afe0a65ac5c376
Mikayla <mikayla.mcewan4@gmail.com>
2019-10-25 14:10:21 +1100

    Create MyOrderAdapter.java

---
+++ app/src/main/java/com/example/a3634homework2/MyOrderAdapter.java
@@ -0,0 +1,38 @@
+package com.example.a3634homework2;
+
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.view.View;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.annotation.NonNull;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import androidx.annotation.NonNull;
+
+public class MyOrderAdapter {
+
+
+
+//    public MyOrderAdapter(HashMap<MenuItem,Integer> order) {
+//        this.order=order;
+//        orderList = (List) Arrays.asList(order.keySet().toArray);
+//    }
+//    @NonNull
+//    @Override
+//    public SelectedItemsViewHolder onCreateViewHolder (ViewGroup parent, int viewType) {
+//        View a = LayoutInflater.from(parent.getContext().inflate(R.layout.my_order_layout, parent, false));
+//                return new SelectedItemsViewHolder(a);
+//
+//    }
+//
+//
+//
+//    private List<MenuItem>  orderList;
+//    private HashMap<MenuItem, Integer> order;
+
+}
